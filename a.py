561313935e2a82b28a99c872b7c8c6dcea51c43d
dannynar3105 <56499150+dannynar3105@users.noreply.github.com>
2019-10-20 17:29:10 +0530

    Add files via upload

--- a.py
+++ a.py
@@ -1,4 +1,4 @@
-import docker
+import docker, requests
 from flask import Flask,request,render_template,jsonify
 import requests, json
 import docker
@@ -8,6 +8,7 @@ app = Flask(__name__,template_folder='.')
 url = "http://0.0.0.0:5000"
 CORS(app)
 cors=CORS(app,resources={r"/api/*": {"origins": "*"}})
+
 
 @app.route('/login')
 def login():
@@ -21,19 +22,61 @@ def Images(vent=False):
       imgs = c.images.list()
       for img in imgs:
         repo_tags = img.tags
+        
+        print (img)
         if not repo_tags:
-            continue
+           continue
         for repo_tag in repo_tags:
                 s_list = repo_tag.split(":")
+                print (s_list)
                 tag = s_list[0]
-                repo = ":".join(s_list[:-1])
-                tag_list.append(tag)
+                print (tag)
+                tag1= s_list[1]
+                repo = tag+":"+tag1
+                print (repo)
+                tag_list.append(repo)
       return json.dumps(tag_list)
     except Exception as e:  # pragma: no cover
               print ("Something with the Images went wrong " + str(e))
               return "no images" 
 
-    
+
+@app.route('/search/', methods=['GET','POST'])
+def search():
+  params2=request.get_data(as_text=True)
+  print ("url is "+request.url)
+  print ("image value =" + params2)
+  try:
+    c = docker.from_env()
+    str1=params2.split(":")
+    pimgval=str1[1].replace("\"","")
+    dimgval=str1[2].replace("\"","")
+    dimgval=dimgval[:-1]
+    imgval=pimgval+":"+dimgval
+    print ("searched image is "+imgval)
+    vallist=c.images.list()
+    for img in vallist:
+        imgsval=img.tags
+        print ("in the search for")
+        for val in img.tags:
+           imgname=val.split(":")
+           print ("inside ..."+str(imgname))
+           pname=imgname[0]
+           tname=imgname[1]
+           print (pname+"==="+tname)
+           print (pimgval+"==="+dimgval)
+           if (pname==pimgval and tname==dimgval):
+              value=img
+              print (value.history())
+              return json.dumps(value.history())
+  except Exception as e:
+       print ("Error in reriveal " + str(e)) 
+       return "no images"
+  return "no images"
+
+
+
+
 
 if __name__ == '__main__':
  app.run(host="0.0.0.0", port = 5000,debug=True)
